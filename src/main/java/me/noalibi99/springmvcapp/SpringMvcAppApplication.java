2c15460cf48914c11de4dc2ec25708b75020d305
Mohammed El Himar <mohamed.elhimar04@gmail.com>
2026-03-08 22:22:18 +0000

    first commit

---
+++ src/main/java/me/noalibi99/springmvcapp/SpringMvcAppApplication.java
@@ -0,0 +1,13 @@
+package me.noalibi99.springmvcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/noalibi99/springmvcapp/SpringMvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.noalibi99.springmvcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
