2cc0073f3f9ee9673933d7c096c7c989e9506915
Mohammed El Himar <mohamed.elhimar04@gmail.com>
2026-03-08 22:22:27 +0000

    first commit

--- src/main/java/me/noalibi99/springmvcapp/SpringMvcAppApplication.java
+++ src/main/java/me/noalibi99/springmvcapp/SpringMvcAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMvcAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcAppApplication.class, args);
+    }
 
 }
--- src/test/java/me/noalibi99/springmvcapp/SpringMvcAppApplicationTests.java
+++ src/test/java/me/noalibi99/springmvcapp/SpringMvcAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringMvcAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
