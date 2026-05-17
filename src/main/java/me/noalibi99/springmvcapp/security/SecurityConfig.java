f8dd5cdaa729b78767404429e8811636e9578269
Mohammed El Himar <mohamed.elhimar04@gmail.com>
2026-05-17 22:50:17 +0100

    secure ressources using spring security, handle roles on views

--- src/main/java/me/noalibi99/springmvcapp/security/SecurityConfig.java
+++ src/main/java/me/noalibi99/springmvcapp/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,16 +15,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public new BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        PasswordEncoder passwordEncoder = passwordEncoder();
         return new InMemoryUserDetailsManager(
-                User.withUsername("user1").password("1234").roles("USER").build(),
-                User.withUsername("user2").password("1234").roles("USER").build(),
-                User.withUsername("admin").password("1234").roles("USER", "ADMIN").build()
+                User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN").build()
 
         );
     }
@@ -32,7 +34,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/index/**").hasRole("USER"))
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/new/**", "/edit/**", "/save/**", "/delete/**").hasRole("ADMIN"))
                 .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .exceptionHandling(eh -> eh.accessDeniedPage("/403"))
                 .build();
     }
 }
--- src/main/java/me/noalibi99/springmvcapp/web/ProductController.java
+++ src/main/java/me/noalibi99/springmvcapp/web/ProductController.java
@@ -1,24 +1,32 @@
 package me.noalibi99.springmvcapp.web;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import me.noalibi99.springmvcapp.entities.Product;
 import me.noalibi99.springmvcapp.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class ProductController {
-    @Autowired
+//    @Autowired
     private ProductRepository productRepository;
     @GetMapping("/")
     public String home() {
         return "redirect:/index";
+    }
+
+    @GetMapping("/403")
+    public String accessDenied() {
+        return "403";
     }
 
     @GetMapping("/index")
@@ -29,7 +37,7 @@ public class ProductController {
     }
 
     @GetMapping("/delete")
-    public String delete(Long id) {
+    public String delete(@RequestParam Long id) {
         productRepository.deleteById(id);
         return "redirect:/index";
     }
@@ -38,6 +46,13 @@ public class ProductController {
     public String newProduct(Model model) {
         model.addAttribute("product", new Product());
         return "new-product";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editProduct(@PathVariable Long id, Model model) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid product Id:" + id));
+        model.addAttribute("product", product);
+        return "edit-product";
     }
 
     @PostMapping("/save")
